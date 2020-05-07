e974a7b896a950a4e6377dca0f1df6c143e851e0
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-05-07 21:59:59 +0100

    Start drawing functions with function to draw a circle

---
+++ js/draw-functions.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const TAU = Math.PI * 2;
+
+
+function drawCircle(p) {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.r, 0, TAU, true);
+    ctx.stroke();
+}
