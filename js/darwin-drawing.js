73fac3e95683cffe14e7f26f7e65c7468e6635a0
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-05-07 23:54:39 +0100

    Add function for draw a line with a smooth curve

--- js/darwin-drawing.js
+++ js/darwin-drawing.js
@@ -1,4 +1,5 @@
 // Entry point for program. Determines what gets drawn
+const showImage = 1;
 
 function drawCreature(genes) {
     const {
@@ -6,6 +7,7 @@ function drawCreature(genes) {
         jaw,
         eye,
         snout,
+        mouth,
     } = genes;
 
     // Build creature
@@ -14,20 +16,39 @@ function drawCreature(genes) {
     getSnoutCenter(jaw, snout);
 
     const nose = getNose(snout, jaw);
+    getMouth(mouth, nose, snout);
 
     // Draw creature
-    ctx.fillStyle = 'none';
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'rgb(255, 0, 0, 0.5)';
-
     drawCircle(head);
     // drawCircle(jaw);
     // drawCircle(snout);
     // drawCircle(eye);
 
-    drawLine(nose.top, snout.top);
-    drawLine(nose.base, snout.base);
-    drawLine(nose.base, nose.top);
+    // drawLine(nose.top, snout.top);
+    // drawLine(nose.base, snout.base);
+    // drawLine(nose.base, nose.top);
+    // drawLine(mouth.start, mouth.end);
+
+    // Draw top of snout
+    let m = lerpPoint(nose.top, mouth.start, 0.49);
+    arcLine(snout.top, nose.top, m, snout.topSharpness);
+    arcLine(m, mouth.start, mouth.end, 0.8);
+
+    // Draw bottom of mouth
+    m = lerpPoint(nose.base, mouth.start, 0.49);
+    arcLine(snout.base, nose.base, m, snout.baseSharpness);
+    arcLine(m, mouth.start, mouth.end, 0.8);
 }
 
-addImage('tiger.jpg', scale=0.3, dx=100, dy=50, () => drawCreature(tiger));
+ctx.fillStyle = 'none';
+ctx.lineWidth = 1;
+ctx.strokeStyle = 'rgb(0, 0, 0, 0.5)';
+drawCreature(tiger);
+
+if (showImage) {
+    ctx.fillStyle = 'none';
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'rgb(255, 0, 0, 0.5)';
+    addImage('tiger.jpg', scale=0.3, dx=50, dy=100, () => drawCreature(tiger));
+}
+
--- js/draw-functions.js
+++ js/draw-functions.js
@@ -20,6 +20,36 @@ function drawLine(p1, p2) {
     ctx.stroke();
 }
 
+// Draw a curved line from p1 to p2 to p3
+function arcLine(p1, p2, p3, curve) {
+    let arm1, arm2, arm3, arm4;
+    if (curve > 0.5) {
+        // Map p back to the range 0 - 1
+        const p = curve * 2 - 1;
+        arm1 = lerpPoint(p1, p2, p);
+        arm4 = lerpPoint(p3, p2, p);
+        
+        // Control arms end at the vertex
+        arm2 = p2;
+        arm3 = p2;
+    } else {
+        arm1 = p1;
+        arm4 = p3;
+        
+        // Control arms move closer to the initial vertices
+        const p = 0.5 + curve;
+        arm2 = lerpPoint(p1, p2, p);
+        arm3 = lerpPoint(p3, p2, p);
+    }
+    
+    ctx.beginPath(); 
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(arm1.x, arm1.y);
+    ctx.bezierCurveTo(arm2.x, arm2.y, arm3.x, arm3.y, arm4.x, arm4.y);
+    ctx.lineTo(p3.x, p3.y);
+    ctx.stroke();
+}
+
 function addImage(filename, scale=1, dx=0, dy=0, callback) {
     const image = new Image();
     image.src = 'images/' + filename;
