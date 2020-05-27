ec845582f831f5df03f862c6609e051c8b196d29
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-05-27 21:52:06 +0100

    Add dot and alongVector functions

--- js/vectors.js
+++ js/vectors.js
@@ -1,3 +1,6 @@
+// Basic vector functions.
+// More complex functions defined in utils
+
 function add(v1, v2) {
     return {
         x: v1.x + v2.x,
@@ -33,6 +36,15 @@ function unitVector(p1, p2) {
     return normalise(minus(p1, p2));
 }
 
+function dot(p1, p2) {
+    return p1.x * p2.x + p1.y * p2.y;  
+}
+
 function lerpPoint(p1, p2, p) {
     return add(p1, multiply(minus(p2, p1), p));
+}
+
+// Start at point p and move s units in the direction of v
+function alongVector(p, v, s) {
+    return add(p, mult(v, s));
 }
